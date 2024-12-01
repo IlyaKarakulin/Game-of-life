5e6553189e2dfd155efbf9518226e7d4623a800d
Ilya <karakulinilia@gmail.com>
2024-12-01 12:27:54 +0700

    make processing mode 0, 1

--- fild_calc.hpp
+++ fild_calc.hpp
@@ -15,16 +15,62 @@ struct pair_hash
     }
 };
 
+class Meta_data
+{
+private:
+    string game_version;
+    string name;
+    string size;
+    string conditions;
+    int game_mode;
+    ofstream fout;
+
+public:
+    Meta_data();
+    ~Meta_data();
+
+    string get_version();
+    string get_name();
+    string get_size();
+    string get_conditions();
+    int get_game_mode();
+
+    void set_game_mode(int mode);
+    void set_version(string game_version);
+    void set_name(string name);
+    void set_size(string size);
+    void set_conditions(string conditional);
+};
+
+class Parse_input_data
+{
+private:
+    int arg_count;
+    char **arg_arr;
+    string in_file;
+    ifstream fin;
+
+public:
+    Parse_input_data(Meta_data &m_data, int args, char **argv);
+    ~Parse_input_data();
+    void parse_head(Meta_data &m_data, string &name, int &size, set<int> &life_condition, set<int> &birth_condition);
+    void parse_field(auto &map_field);
+    int get_size();
+    string get_input_file();
+    void normalize_command(string &command);
+};
+
 class Field_calculation
 {
 public:
-    Field_calculation(int argc, char **argv);
+    Field_calculation(Meta_data &m_data, Parse_input_data &parser, int argc, char **argv);
     ~Field_calculation();
-    void calc_iter(auto &map_field);
+    void calc_iter();
     void check_del(auto &map_field, forward_list<pair<int, int>> &cell_on_del, auto &cell_on_check_birth);
     void check_birth(auto &map_field, auto &cell_on_check_birth, forward_list<pair<int, int>> &cell_on_birth);
     void check_neubors(auto &map_field, auto &cell_on_check_birth, pair<int, int> cell);
-    unordered_map<pair<int, int>, int, pair_hash> map_field; //!!!!!!!
+    int get_size();
+    forward_list<pair<int, int>> get_cells();
 
 private:
     int size;
@@ -32,4 +78,29 @@ private:
     set<int> set_survival_condition;
     set<int> set_birth_condition;
     vector<vector<bool>> field;
+    unordered_map<pair<int, int>, int, pair_hash> map_field;
+};
+
+class Print_field
+{
+private:
+    int size;
+    vector<vector<bool>> matrix_field;
+
+public:
+    void print(Field_calculation &map_field);
+    void save(Field_calculation &map_field, Meta_data &m_data, string &file_name);
+    void clear(int lines);
+    void print_help();
+    Print_field(Field_calculation &map_field);
+    ~Print_field();
+};
+
+class Game_process
+{
+public:
+    Game_process();
+    ~Game_process();
+    void start_game(int argc, char **argv);
+    bool mode0_1(Field_calculation &game, Print_field &printer, Meta_data &m_data, string command, string output_file_name);
 };
--- main.cpp
+++ main.cpp
@@ -6,12 +6,72 @@
 #include <tuple>
 #include <forward_list>
 #include <array>
+#include <cstdlib>
+// #include <boost/algorithm/string.hpp>
+#include <string>
 #include "fild_calc.hpp"
-#include "parser.hpp"
 
 using namespace std;
 
-Parse_input_data::Parse_input_data(int args, char **argv)
+Meta_data::Meta_data()
+{
+}
+
+Meta_data::~Meta_data()
+{
+}
+
+void Meta_data::set_version(string game_version)
+{
+    this->game_version = game_version;
+}
+
+void Meta_data::set_name(string name)
+{
+    this->name = name;
+}
+
+void Meta_data::set_size(string size)
+{
+    this->size = size;
+}
+
+void Meta_data::set_conditions(string conditional)
+{
+    this->conditions = conditions;
+}
+
+void Meta_data::set_game_mode(int mode)
+{
+    this->game_mode = mode;
+}
+
+int Meta_data::get_game_mode()
+{
+    return this->game_mode;
+}
+
+string Meta_data::get_version()
+{
+    return this->game_version;
+}
+
+string Meta_data::get_name()
+{
+    return this->name;
+}
+
+string Meta_data::get_size()
+{
+    return this->size;
+}
+
+string Meta_data::get_conditions()
+{
+    return this->conditions;
+}
+
+Parse_input_data::Parse_input_data(Meta_data &m_data, int args, char **argv)
 {
     this->arg_count = args;
     this->arg_arr = argv;
@@ -19,15 +79,15 @@ Parse_input_data::Parse_input_data(int args, char **argv)
     if (args == 1)
     {
         this->in_file = "./default_field.live";
-        game_mode = 0;
+        m_data.set_game_mode(0);
     }
     else
     {
         this->in_file = this->arg_arr[1];
         if (args == 2)
-            game_mode = 1;
+            m_data.set_game_mode(1);
         else
-            game_mode = 2;
+            m_data.set_game_mode(2);
     }
 }
 
@@ -40,21 +100,25 @@ string Parse_input_data::get_input_file()
     return this->in_file;
 }
 
-void Parse_input_data::parse_head(string &name, int &size, set<int> &set_birth_condition, set<int> &set_survival_condition)
+void Parse_input_data::parse_head(Meta_data &m_data, string &name, int &size, set<int> &set_birth_condition, set<int> &set_survival_condition)
 {
     fin.open(get_input_file());
 
-    string format;
-    getline(fin, format); // TODO add processing exeptions. if format isn't 1.06
+    string version;
+    getline(fin, version); // TODO add processing exeptions. if version isn't 1.06
+    m_data.set_version(version);
 
     getline(fin, name);
+    m_data.set_name(name);
     name = name.substr(3);
 
     string tmp;
     getline(fin, tmp);
+    m_data.set_size(tmp);
     size = stoi(tmp.substr(3));
 
     getline(fin, tmp);
+    m_data.set_conditions(tmp);
     tmp = tmp.substr(4);
     int i = 0;
 
@@ -77,23 +141,29 @@ void Parse_input_data::parse_field(auto &map_field)
 {
     pair<int, int> key;
 
-    for (int i = 0; i < 3; ++i) // TODO make condition to for
+    while (fin >> key.second)
     {
-        fin >> key.first;
         fin.ignore();
-        fin >> key.second;
+        fin >> key.first;
 
         if (map_field.count(key))
             cerr << "Warning! the cell (" << key.first << ' ' << key.second << ") has already been created before";
         else
             map_field[key] = 0;
     }
+
+    this->fin.close();
 }
 
-Field_calculation::Field_calculation(int argc, char **argv)
+void Parse_input_data::normalize_command(string &command)
 {
-    Parse_input_data parser(argc, argv);
-    parser.parse_head(this->game_name, this->size, this->set_birth_condition, this->set_survival_condition);
+    while (!command.empty() && isspace(command.back()))
+        command.pop_back();
+}
+
+Field_calculation::Field_calculation(Meta_data &m_data, Parse_input_data &parser, int argc, char **argv)
+{
+    parser.parse_head(m_data, this->game_name, this->size, this->set_birth_condition, this->set_survival_condition);
 
     // cout << this->game_name << endl
     //      << this->size << endl;
@@ -105,10 +175,6 @@ Field_calculation::Field_calculation(int argc, char **argv)
     //     cout << x << ' ';
     // cout << endl
     //      << "--------" << endl;
-
-    // this->field.reserve(size); //?????
-    // for (vector<bool> &x : this->field)
-    //     x.assign(size, false);
 
     parser.parse_field(this->map_field);
 
@@ -122,6 +188,21 @@ Field_calculation::Field_calculation(int argc, char **argv)
 
 Field_calculation::~Field_calculation()
 {
+}
+
+int Field_calculation::get_size()
+{
+    return this->size;
+}
+
+forward_list<pair<int, int>> Field_calculation::get_cells()
+{
+    forward_list<pair<int, int>> cells;
+
+    for (const auto &[cell, _] : this->map_field)
+        cells.push_front(cell);
+
+    return cells;
 }
 
 void Field_calculation::check_neubors(auto &map_field, auto &cell_on_check_birth, pair<int, int> cell)
@@ -164,29 +245,212 @@ void Field_calculation::check_birth(auto &map_field, auto &cell_on_check_birth, 
     }
 }
 
-void Field_calculation::calc_iter(auto &map_field)
+void Field_calculation::calc_iter()
 {
     forward_list<pair<int, int>> cell_on_del;
     forward_list<pair<int, int>> cell_on_birth;
 
     unordered_map<pair<int, int>, int, pair_hash> cell_on_check_birth;
 
-    check_del(map_field, cell_on_del, cell_on_check_birth);
-    check_birth(map_field, cell_on_check_birth, cell_on_birth);
+    check_del(this->map_field, cell_on_del, cell_on_check_birth);
+    check_birth(this->map_field, cell_on_check_birth, cell_on_birth);
 
-    cout << "To del:\n";
     for (const auto &cell : cell_on_del)
-        cout << "(" << cell.first << " " << cell.second << ")" << endl;
+        this->map_field.erase(cell);
 
-    cout << "Mb birth\n";
+    for (const auto &[cell, count] : this->map_field)
+        this->map_field[cell] = 0;
+
     for (const auto &cell : cell_on_birth)
-        cout << "(" << cell.first << " " << cell.second << ")" << endl;
+        this->map_field[cell] = 0;
+
+    // cout << "To del:\n";
+    // for (const auto &cell : cell_on_del)
+    //     cout << "(" << cell.first << " " << cell.second << ")" << endl;
+
+    // cout << "Mb birth\n";
+    // for (const auto &cell : cell_on_birth)
+    //     cout << "(" << cell.first << " " << cell.second << ")" << endl;
+
+    //* for (const auto &[key, value] : this->map_field)
+    //*     cout << "(" << key.first << ' ' << key.second << ")" << " - " << value << endl;
+}
+
+Print_field::Print_field(Field_calculation &map_field)
+{
+    this->size = map_field.get_size();
+
+    this->matrix_field.reserve(this->size);
+
+    for (int i = 0; i < this->size; ++i)
+        matrix_field.push_back(vector<bool>(this->size, false));
+}
+
+Print_field::~Print_field()
+{
+}
+
+void Print_field::print(Field_calculation &map_field)
+{
+    for (const pair<int, int> cell : map_field.get_cells())
+        matrix_field[cell.first][cell.second] = true;
+
+    for (int i = 0; i < this->size; ++i)
+    {
+        for (int j = 0; j < this->size; ++j)
+        {
+            cout << (matrix_field[i][j] ? "# " : ". ");
+            matrix_field[i][j] = false;
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void Print_field::save(Field_calculation &map_field, Meta_data &m_data, string &file_name)
+{
+    ofstream fout(file_name);
+    fout << m_data.get_version() << endl;
+    fout << m_data.get_name() << endl;
+    fout << m_data.get_size() << endl;
+    fout << m_data.get_conditions() << endl;
+
+    for (const auto &[x, y] : map_field.get_cells())
+    {
+        fout << y << " " << x << endl;
+    }
+
+    fout.close();
+}
+
+void Print_field::clear(int lines)
+{
+    for (int i = 0; i < lines; ++i)
+        std::cout << "\033[F\033[K";
+}
+
+void Print_field::print_help()
+{
+    cout << "You can specify the name of the output file and the number of steps at startup:" << endl;
+    cout << "./game <name input file> -i <count step> -o <name out file>" << endl
+         << endl;
+    cout << "Or play step by step. At startup, you can transfer a file that" << endl;
+    cout << "describes the field in the live 1.06 format." << endl;
+    cout << "if you do not transfer the file, the default field opens" << endl
+         << endl;
+    cout << "Commants to play step by step:" << endl;
+    cout << " - dump <out file> (by default, it is saved in out.live)" << endl;
+    cout << " - tick <n> (by default n = 1)" << endl;
+    cout << " - exit" << endl;
+}
+
+Game_process::Game_process()
+{
+}
+
+Game_process::~Game_process()
+{
+}
+
+bool Game_process::mode0_1(Field_calculation &game, Print_field &printer, Meta_data &m_data, string command, string output_file_name)
+{
+    static int count_str_to_del = 0;
+    int count_tick = 1;
+    bool flag = true;
+
+    if (command.starts_with("dump"))
+    {
+        printer.clear(1);
+
+        if (command.length() == 4)
+        {
+            printer.save(game, m_data, output_file_name);
+        }
+        else if (!command.ends_with(".live") || (command.length() < 11)) // "dump ?.live" min correct command (len = 11)
+        {
+            cerr << "Error! Incorrect file format" << endl;
+            ++count_str_to_del;
+        }
+        else
+        {
+            output_file_name = command.substr(5);
+            printer.save(game, m_data, output_file_name);
+        }
+    }
+    else if (command.starts_with("tick"))
+    {
+        printer.clear(game.get_size() + 2 + count_str_to_del);
+        count_str_to_del = 0;
+
+        if (command.length() > 5)
+            count_tick = stoi(command.substr(5));
+
+        for (int i = 0; i < count_tick; ++i)
+            game.calc_iter();
+
+        printer.print(game);
+    }
+    else if (command == "exit")
+    {
+        printer.clear(1 + count_str_to_del);
+        count_str_to_del = 0;
+
+        flag = false;
+    }
+    else if (command == "help")
+    {
+        printer.clear(1 + count_str_to_del);
+        count_str_to_del = 0;
+
+        printer.print_help();
+
+        cout << endl
+             << "press ENTER to continue...";
+        // cin.ignore();
+        cin.get();
+        printer.clear(13);
+    }
+    else
+    {
+        printer.clear(count_str_to_del);
+        count_str_to_del = 0;
+
+        cout << "Error!" << endl
+             << command << " not found" << endl;
+        count_str_to_del += 3;
+    }
+
+    return flag;
+}
+
+void Game_process::start_game(int argc, char **argv)
+{
+    Meta_data m_data;
+    Parse_input_data parser(m_data, argc, argv);
+    Field_calculation game(m_data, parser, argc, argv);
+    Print_field printer(game);
+    string command;
+    const string output_file_name = "out.live";
+    int count_tick = 1;
+    bool flag = true;
+
+    printer.print(game);
+
+    while (flag)
+    {
+        if (m_data.get_game_mode() != 2)
+        {
+            getline(cin, command);
+            parser.normalize_command(command);
+            flag = mode0_1(game, printer, m_data, command, output_file_name);
+        }
+    }
 }
 
 int main(int argc, char **argv)
 {
-    Field_calculation game(argc, argv);
-    game.calc_iter(game.map_field);
+    Game_process game;
+    game.start_game(argc, argv);
 
     return 0;
 }
